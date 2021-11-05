742e7b91d1fa1ec75aa3f64ea1d578f1250e8f50
hi02102002e <2051120240@ut.edu.vn>
2021-11-05 17:45:13 +0700

    slide

--- js/app.js
+++ js/app.js
@@ -1,6 +1,49 @@
-var swiper = new Swiper('.mySwiper', {
+var swiperBanner = new Swiper('.mySwiper', {
+  speed: 500,
   pagination: {
     el: '.swiper-pagination',
     dynamicBullets: true,
+    clickable: true,
+  },
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false,
+  },
+  loop: true,
+});
+
+var swiper = new Swiper('.tours__swiper', {
+  spaceBetween: 30,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    dynamicBullets: true,
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  loop: true,
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 4,
+      spaceBetween: 30,
+    },
   },
 });
